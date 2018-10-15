6fa3dfa73639365cd783bc18dd023336a2b2dbe9
Go-Pomegranate <43642774+Go-Pomegranate@users.noreply.github.com>
2018-10-15 10:18:30 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,20 +10,6 @@ import (
 
 func main() {
 
-	//mail := models.Mail{Sender:"postmaster@sandboxdf5be3d8493a4c6e89ed2cb5ed724f65.mailgun.org", Receiver: "eryk.panter@gmail.com", Content:"Hello from the other side!"}
-	//
-	////mailService := services.MailServiceWithCircuitBreaker{}
-	//
-	//mailService := services.MailService{}
-	//
-	//result, err := mailService.SendMail(mail)
-	//
-	//if err != nil {
-	//	log.Printf("%+v\n", err)
-	//}
-	//
-	//log.Println(result)
-
 	router := routers.NewRouter()
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
